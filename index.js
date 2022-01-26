22d1b842a4705e2c72df047c18c82fffd8902880
Janne Korkee <janne.korkee@eficode.com>
2022-01-26 15:14:12 +0200

    added more bugs and vulns

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-const passwd="passw0rd"
+const passwd="metsäGr0uppiRuulz"
 console.log(passwd)
+
+const switchi="a"
+switch(switchi) {
+    case "a":
+      console.log("switchi oli A")
+      break;
+    case "b":
+        console.log("switchi oli B")
+      break;
+  }
+  //tämä kannattaisi tehdä vaa ihan IF:llä jos kerran vain 2 mahdollisuutta
+
+
+let turhapuro = "uuno"
+//unused variable. LET = muuttuva variable toisinkuin const
+
+
+const nulli = null 
+console.log(nulli)
+//null variableja ei varmaan kannata koittaa saada ruudulle...
+
+
+function simppeli(x) {
+    return x
+  }
+
+console.log(simppeli("helloo",2))
+//ei kannata varmaan antaa tota 2 parametriä tohon ku ei toi funktio ota ku ekan vastaan...
+
+
