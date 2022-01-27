ae43482ece1cdb6ca1882bac824b2caa3f7d6543
Janne Korkee <janne.korkee@eficode.com>
2022-01-27 11:45:39 +0200

    randerom

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ const nulli = null
 console.log(nulli)
 //null variableja ei varmaan kannata koittaa saada ruudulle...
 
+
+const switchi="a"
+switch(switchi) {
+    case "a":
+      console.log("switchi oli A")
+      break;
+    case "b":
+        console.log("switchi oli B")
+      break;
+  }
+  //tämä kannattaisi tehdä vaa ihan IF:llä jos kerran vain 2 mahdollisuutta
+
+
+let turhapuro = "uuno"
+//unused variable. LET = muuttuva variable toisinkuin const
+
+
+const nulli = null 
+console.log(nulli)
+//null variableja ei varmaan kannata koittaa saada ruudulle...
+
