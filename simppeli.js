810a9a07dbe22801d1d2760529e831ef4fc7daac
Janne Korkee <janne.korkee@eficode.com>
2022-01-27 09:39:06 +0200

    added duplicate code

--- simppeli.js
+++ simppeli.js
@@ -1,11 +1,26 @@
 
-
 function simppeli(x,y) {
     return x + y
   }
 
-
 console.log(simppeli(1,2, 3))
 //ei kannata varmaan antaa tota 2 parametriä tohon ku ei toi funktio ota ku ekan vastaan...
+
+
+//DUPLACTE CODE:
+const jotain = "jotain"
+
+if (jotain === "jotain") {
+    const tulos = jotain + "extraa"
+    const tulos2 = jotain + "extraa2"
+    console.log(tulos + tulos2)
+}
+
+if (jotain === "jotain") {
+    const tulos = jotain + "extraa"
+    const tulos2 = jotain + "extraa2"
+    console.log(tulos + tulos2)
+}
+
 
 module.exports = simppeli
